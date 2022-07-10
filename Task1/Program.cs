481b42782d5117ee35cdc66d9206f0d8f83c962f
Turin1984 <turin84@mail.ru>
2022-07-10 21:29:27 +0300

    Задача 1: Задайте значения M и N. Напишите программу, которая выведет все чётные натуральные числа в промежутке от M до N с помощью рекурсии.

---
+++ Task1/Program.cs
@@ -0,0 +1,25 @@
+﻿/*Задайте значения M и N. Напишите программу, которая выведет все чётные натуральные числа 
+в промежутке от M до N с помощью рекурсии. */
+
+Console.Clear();
+int Prompt(string message)
+{
+Console.Write(message);
+string a_String = Console.ReadLine();
+return int.Parse(a_String);
+}
+void PrintArray(int M, int N)
+{
+if (M > N) return;
+PrintArray(M, N-1);
+if (N%2==0) System.Console.Write($"{N}; ");
+}
+
+int M = Prompt("Введите число M: ");
+int N = Prompt("Введите число N: ");
+
+System.Console.WriteLine();
+
+System.Console.Write($"M = {M}, N = {N} : ");
+PrintArray(M, N);
+System.Console.WriteLine();
